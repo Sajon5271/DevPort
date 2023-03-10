79cfc1cddcca371e35dba2394f159613dc58f2d4
Sajon <sajon5271@gmail.com>
2023-03-10 12:33:25 +0600

    Changed line endings to lf from crlf

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose');
 const router = require('./router');
-const app = express();
 const fileUpload = require('express-fileupload')
 const PORT = 3000;
 
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
