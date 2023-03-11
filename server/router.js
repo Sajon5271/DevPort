c47af5c8887097a4aee70d13334b382cb4a08a71
Sajon <sajon5271@gmail.com>
2023-03-11 20:39:59 +0600

    Router auth middleware added

--- server/router.js
+++ server/router.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const profileController = require('./controllers/profile');
 const userController = require('./controllers/user');
+const authMiddleware = require('./middleware/auth');
 
 router.get('/dashboard', profileController.getAllProfiles);
-router.put('/dashboard', profileController.updateProfile);
+router.put('/dashboard', authMiddleware, profileController.updateProfile);
 router.get('/dashboard/:id', profileController.getSingleProfile);
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
